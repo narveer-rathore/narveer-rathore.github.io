db7e0fc61a4b715dc82b33d535490e74b3b12d55
Narveer Rathore <rathore.narveer1994+1@gmail.com>
2020-09-13 17:28:02 +0530

    cache update

---
+++ campaigns/precache-manifest.dedb886a127aa69488d25555f28e36f6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a100c746d076ac7dda2ff507827dcc1e",
+    "url": "/campaigns/index.html"
+  },
+  {
+    "revision": "a6c362bd95596b82b9e2",
+    "url": "/campaigns/static/css/2.8d4cdce2.chunk.css"
+  },
+  {
+    "revision": "107c8922c3b4a98034dd",
+    "url": "/campaigns/static/css/main.2a523cd0.chunk.css"
+  },
+  {
+    "revision": "a6c362bd95596b82b9e2",
+    "url": "/campaigns/static/js/2.dfe2e710.chunk.js"
+  },
+  {
+    "revision": "7ab12ae16a37224b7b54ce36b00f9c85",
+    "url": "/campaigns/static/js/2.dfe2e710.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "107c8922c3b4a98034dd",
+    "url": "/campaigns/static/js/main.da5fa4e9.chunk.js"
+  },
+  {
+    "revision": "1cc559b46559ed095cae",
+    "url": "/campaigns/static/js/runtime-main.af90f7fc.js"
+  },
+  {
+    "revision": "44ec242a0161affd3f870149560b795c",
+    "url": "/campaigns/static/media/default-game-thumbnail.44ec242a.png"
+  },
+  {
+    "revision": "0849cabdc8e5fa886512971ae2311e82",
+    "url": "/campaigns/static/media/icomoon.0849cabd.ttf"
+  },
+  {
+    "revision": "9bd966ad2e239bbdeb8a0ce3cc82ba29",
+    "url": "/campaigns/static/media/icomoon.9bd966ad.woff"
+  },
+  {
+    "revision": "da8420d5bedc1cf754465d2d06bd3a60",
+    "url": "/campaigns/static/media/icomoon.da8420d5.eot"
+  },
+  {
+    "revision": "f66de82e20eb70bd9fba420a86be57db",
+    "url": "/campaigns/static/media/icomoon.f66de82e.svg"
+  }
+]);
--- campaigns/service-worker.js
+++ campaigns/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.81690a4fe82f3c400c7efb89289e4bdf.js"
+  "/campaigns/precache-manifest.dedb886a127aa69488d25555f28e36f6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/campaigns/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
