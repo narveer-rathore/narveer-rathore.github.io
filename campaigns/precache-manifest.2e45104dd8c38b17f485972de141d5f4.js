fc5d248c632e09dc6609a2817817eea02b0805dc
Narveer Rathore <rathore.narveer1994+1@gmail.com>
2020-09-13 17:22:10 +0530

    added campaigns

---
+++ campaigns/precache-manifest.2e45104dd8c38b17f485972de141d5f4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45373ac473508732239a92763b2a552d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6c362bd95596b82b9e2",
+    "url": "/static/css/2.8d4cdce2.chunk.css"
+  },
+  {
+    "revision": "5c344763a3c5dc6afd27",
+    "url": "/static/css/main.e8ef7f1d.chunk.css"
+  },
+  {
+    "revision": "a6c362bd95596b82b9e2",
+    "url": "/static/js/2.dfe2e710.chunk.js"
+  },
+  {
+    "revision": "7ab12ae16a37224b7b54ce36b00f9c85",
+    "url": "/static/js/2.dfe2e710.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c344763a3c5dc6afd27",
+    "url": "/static/js/main.49a0ad07.chunk.js"
+  },
+  {
+    "revision": "6fa616e3c0f6a4247221",
+    "url": "/static/js/runtime-main.d7f71e56.js"
+  },
+  {
+    "revision": "44ec242a0161affd3f870149560b795c",
+    "url": "/static/media/default-game-thumbnail.44ec242a.png"
+  },
+  {
+    "revision": "0849cabdc8e5fa886512971ae2311e82",
+    "url": "/static/media/icomoon.0849cabd.ttf"
+  },
+  {
+    "revision": "9bd966ad2e239bbdeb8a0ce3cc82ba29",
+    "url": "/static/media/icomoon.9bd966ad.woff"
+  },
+  {
+    "revision": "da8420d5bedc1cf754465d2d06bd3a60",
+    "url": "/static/media/icomoon.da8420d5.eot"
+  },
+  {
+    "revision": "f66de82e20eb70bd9fba420a86be57db",
+    "url": "/static/media/icomoon.f66de82e.svg"
+  }
+]);
